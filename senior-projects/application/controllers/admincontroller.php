18a7b78f47dfaf969a7c7fac4b174c1e68f18938
mgarc152 <mgarc152@jccl-15.ad.cs.fiu.edu>
2013-11-03 15:25:16 -0500

    updated admincontroller to support the admin dashboard display (create professors, change user statuses)

--- senior-projects/application/controllers/admincontroller.php
+++ senior-projects/application/controllers/admincontroller.php
@@ -8,6 +8,8 @@ class AdminController extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->helper('flash_message');
+        $this->load->model('spw_user_model');
     }
 
     public function index() {
@@ -55,6 +57,79 @@ class AdminController extends CI_Controller {
             }
         }
         $this->load->view('login_index', $data);
+    }
+    
+     public function admin_dashboard()
+    {
+        $this -> load -> view('admin_dashboard');
+    }
+    
+    public function register_professor()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('email_address', 'Email Address','required|valid_email'); 
+        $this->form_validation->set_rules('password_1', 'Password','required|min_length[6]'); 
+        $this->form_validation->set_rules('password_2', 'Password','required|min_length[6]'); 
+        $data = array(); 
+
+        if($this->form_validation->run() !== false)
+        {
+            $this->load->model('spw_user_model');
+
+            $res = $this->spw_user_model->is_spw_registered($this->input->post('email_address'));
+            if($res == false)
+            {
+                $this->spw_user_model->create_new_professor_user($this->input->post('email_address'), $this->input->post('password_1'));
+                $msg = 'Successfully created a new professor user with the email: '. $this->input->post('email_address');
+                setFlashMessage($this, $msg);
+                /*
+                $sess_array = array(
+                    'id' => $new_user_id, 
+                    'email' => $this->input->post('email_address'), 
+                    'using' => 'fiu_senior_project',
+                );
+                $this->session->set_userdata('logged_in', $sess_array);
+            
+                redirect('user','refresh');*/
+            }
+            else
+            {
+                $msg = 'ERROR: Cannot create a professor with the email: '. $this->input->post('email_address') . ' 
+                    <br>User with this email already exists';
+                setFlashMessage($this, $msg);
+                $data['already_registered'] = true;
+            }
+        }
+         redirect('admin/admin_dashboard');
+    }
+    
+    //need a fucntion that will retrieve all the users that are currently in the system
+    public function activate_deactive_users()
+    {
+        if ($this->input->post('action') === 'Deactivate'){
+            if (is_array($this->input->post('users')))
+            {
+                //retrieve all the ids from the array
+                foreach ($this->input->post('users') as $key => $value)
+                {
+                    $this-> spw_user_model -> change_status_to_inactive($value);
+                }       
+           } 
+        }
+        
+        else if ($this->input->post('action') === 'Activate')
+        {
+            if (is_array($this->input->post('users')))
+            {
+                //retrieve all the ids from the array
+                foreach ($this->input->post('users') as $key => $value)
+                {
+                    $this-> spw_user_model -> change_status_to_active($value);
+                }       
+           } 
+        }
+        
+        redirect('admin/admin_dashboard');
     }
 
 }
